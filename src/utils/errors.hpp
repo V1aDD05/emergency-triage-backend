93b06b7fc6bddb7f8902655373033c42c6815940
ivanushkinvi <ivanushkinvi@gmail.com>
2026-06-16 16:59:50 +0300

    refactor(exception class): updated ValidationError class`s fields and methods

--- src/utils/errors.hpp
+++ src/utils/errors.hpp
@@ -4,16 +4,14 @@
 #include <string>
 
 class ValidationError : public std::exception {
-public:
-    std::string field_;
-    std::string message_;
-    mutable std::string full_message_;   
-
-    ValidationError(const std::string &field, const std::string &message)
-        : field_(field), message_(message) {}
+public:  
+    ValidationError(const std::string &field, const std::string &message) {
+        message_ = "Field '" + field + "': " + message;
+    }
 
     const char* what() const noexcept override {
-        full_message_ = "Field '" + field_ + "': " + message_;
-        return full_message_.c_str();
+        return message_.c_str();
     }
+private:
+    std::string message_;
 };
