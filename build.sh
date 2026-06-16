69a6085de6bd67a12a4fb6b02c33426c28479aa6
ivanushkinvi <ivanushkinvi@gmail.com>
2026-06-16 16:00:35 +0300

    chore(scripts): update shebang to use /usr/bin/env bash

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 cmake --preset gcc-debug
 cmake --build --preset build-debug
--- tests/scripts/test_api.sh
+++ tests/scripts/test_api.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 BASE_URL="http://localhost:8080"
 
