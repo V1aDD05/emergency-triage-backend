8c20a1cb0d33e03c68f3c467c8ca3ef69927c3f3
ivanushkinvi <ivanushkinvi@gmail.com>
2026-06-18 14:29:51 +0300

    refactor(data structures): moved data structures definition to separate file
    - File src/storage/data_structures.hpp now contains all data structures definitions
    - File src/storage/storage.hpp now contains only in-memory storage definition

---
+++ src/storage/data_structures.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <chrono>
+#include <cstdint>
+#include <optional>
+
+enum class Gender : uint8_t { Male, Female };
+
+enum class PatientStatus : uint8_t { OnTheWay, Waiting, InSurgery, IntensiveCare, Died };
+
+struct Patient {
+    std::chrono::system_clock::time_point timestamp_;
+    uint32_t mask_;
+    uint32_t id_;
+    uint8_t priority_;
+    PatientStatus status_;
+    std::optional<uint8_t> age_;
+    std::optional<Gender> sex_;
+
+    Patient(const std::chrono::system_clock::time_point &timestamp, uint32_t mask, uint32_t id, uint8_t priority,
+            PatientStatus status, std::optional<uint8_t> age = std::nullopt, std::optional<Gender> sex = std::nullopt)
+        : timestamp_(timestamp), mask_(mask), id_(id), priority_(priority), status_(status), age_(age), sex_(sex) {
+    }
+};
--- src/storage/storage.hpp
+++ src/storage/storage.hpp
@@ -1,28 +1,11 @@
 #pragma once
-#include <chrono>
+
 #include <cstdint>
 #include <optional>
 #include <unordered_map>
 #include <vector>
 
-enum class Gender : uint8_t { Male, Female };
-
-enum class PatientStatus : uint8_t { OnTheWay, Waiting, InSurgery, IntensiveCare, Died };
-
-struct Patient {
-    std::chrono::system_clock::time_point timestamp_;
-    uint32_t mask_;
-    uint32_t id_;
-    uint8_t priority_;
-    PatientStatus status_;
-    std::optional<uint8_t> age_;
-    std::optional<Gender> sex_;
-
-    Patient(const std::chrono::system_clock::time_point &timestamp, uint32_t mask, uint32_t id, uint8_t priority,
-            PatientStatus status, std::optional<uint8_t> age = std::nullopt, std::optional<Gender> sex = std::nullopt)
-        : timestamp_(timestamp), mask_(mask), id_(id), priority_(priority), status_(status), age_(age), sex_(sex) {
-    }
-};
+#include "data_structures.hpp"
 
 class PatientStorage {
 public:
