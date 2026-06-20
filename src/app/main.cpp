0600b46a15273d87aea285d037e297a95e9cd1d8
ivanushkinvi <ivanushkinvi@gmail.com>
2026-06-20 17:27:36 +0300

    feat: create variables containing server host and server port in main.cpp

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -6,10 +6,12 @@
 #include "emergency_triage/storage/storage.hpp"
 
 int main() {
+	const char* host = std::getenv("SERVER_HOST") ? std::getenv("SERVER_HOST") : "127.0.0.1";
+	int port = std::getenv("SERVER_PORT") ? std::stoi(std::getenv("SERVER_PORT")) : 8080;
 	emergency_triage::PatientStorage storage;
 	httplib::Server server;
     setupHandlers(server, storage);
-    std::cout << "Server started on http://localhost:8080" << std::endl;
-    server.listen("0.0.0.0", 8080);
-    return 0;
+    std::cout << "Server started on "<< host << ":" << port << std::endl;
+	server.listen(host, port);
+	return 0;
 }
